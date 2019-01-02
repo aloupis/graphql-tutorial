b227061ba5861951d37f12500e426d804181a682
aloupis <manos.aloupis@gmail.com>
2019-01-02 13:52:48 +0200

    Section 2 - GraphQL Basics: Schemas and Queries

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,10 +1,74 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer } from "graphql-yoga";
 
 // Scalar types - String, Boolean, Int, Float, ID
+
+//Demo User data
+const users = [
+  {
+    id: "1",
+    name: "Chupacabra",
+    email: "chupacabra026@gmail.com",
+    age: 28
+  },
+  {
+    id: "2",
+    name: "Firebrand",
+    email: "firebrand@hotmail.com"
+  },
+  {
+    id: "3",
+    name: "Aloupis",
+    email: "manos.aloupis@gmail.com"
+  }
+];
+
+// Demo Post Data
+
+const posts = [
+  {
+    id: "10",
+    title: "Why do we use it?",
+    body:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    published: true,
+    author: "1"
+  },
+  {
+    id: "11",
+    title: "What is Lorem Ipsum?",
+    body:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    published: true,
+    author: "1"
+  },
+  {
+    id: "12",
+    title: "Where can I get some?",
+    body:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    published: false,
+    author: "3"
+  }
+];
+
+const comments = [
+  {
+    id: "100",
+    text: "This worked well for me. Thanks!",
+    author: "2",
+    post: "10"
+  },
+  { id: "101", text: "Glad you enjoyed it", author: "1", post: "10" },
+  { id: "102", text: "This did not work", author: "3", post: "11" },
+  { id: "103", text: "Nevermind. I got it to work.", author: "3", post: "11" }
+];
 
 // Type definitions (schema)
 const typeDefs = `
     type Query {
+      users(query: String) : [User!]!
+      posts(query: String) : [Post!]!
+      comments : [Comment!]!
         me: User!
         post: Post!
     }
@@ -14,6 +78,8 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
     }
 
     type Post {
@@ -21,35 +87,102 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+        comments: [Comment!]!
     }
-`
+
+    type Comment{
+      id:ID!
+      text:String!
+      author:User!
+      post: Post!
+    }
+`;
 
 // Resolvers
 const resolvers = {
-    Query: {
-        me() {
-            return {
-                id: '123098',
-                name: 'Mike',
-                email: 'mike@example.com'
-            }
-        },
-        post() {
-            return {
-                id: '092',
-                title: 'GraphQL 101',
-                body: '',
-                published: false
-            }
-        }
+  Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+      return posts.filter(post => {
+        return (
+          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.body.toLowerCase().includes(args.query.toLowerCase())
+        );
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments;
+    },
+    me() {
+      return {
+        id: "123098",
+        name: "Mike",
+        email: "mike@example.com"
+      };
+    },
+    post() {
+      return {
+        id: "092",
+        title: "GraphQL 101",
+        body: "",
+        published: false
+      };
     }
-}
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find(user => {
+        return user.id === parent.author;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => {
+        return comment.post === parent.id;
+      });
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => {
+        return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => {
+        return comment.author === parent.id;
+      });
+    }
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find(user => {
+        return user.id == parent.author;
+      });
+    },
+    post(parent, args, ctx, info) {
+      return posts.find(post => {
+        return post.id === parent.post;
+      });
+    }
+  }
+};
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers
+});
 
 server.start(() => {
-    console.log('The server is up!')
-})
+  console.log("The server is up!");
+});
