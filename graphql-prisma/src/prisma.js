77649206df5669027c0a6f70c2cce9f8ae3170e7
aloupis <manos.aloupis@gmail.com>
2019-01-13 16:12:18 +0200

    Lecture 54

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -6,6 +6,12 @@ const prisma = new Prisma({
 });
 
 const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({ id: authorId });
+
+  if (!userExists) {
+    throw Error("User not found!");
+  }
+
   const post = await prisma.mutation.createPost(
     {
       data: {
@@ -17,43 +23,46 @@ const createPostForUser = async (authorId, data) => {
         }
       }
     },
-    "{ id title body author { id name }}"
+    "{ author {id name email posts{id title published}}}"
   );
 
-  const user = await prisma.query.user(
-    { where: { id: authorId } },
-    "{id name email posts{id title published}}"
-  );
-  return user;
+  return post.author;
 };
 
 const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({ id: postId });
+
+  if (!postExists) {
+    throw new Error("Post does not exist!");
+  }
   const post = await prisma.mutation.updatePost(
     {
       where: { id: postId },
       data
     },
-    "{ author { id } }"
+    "{ author {id name email posts{id title body published}} }"
   );
 
-  const user = await prisma.query.user(
-    { where: { id: post.author.id } },
-    "{id name email posts{id title published}}"
-  );
-  return JSON.stringify(user);
+  return post.author;
 };
 
-// updatePostForUser("cjqtjn3xf00110a31bfmlfvn1", {
-//   title: "GraphQl updated",
-//   body: `Updated graphql at ${new Date()} !!`
-// }).then(user => {
-//   console.log(JSON.stringify(user, undefined, 2));
-// });
+updatePostForUser("cjqtjn3xf00110a31bfmlfvn1", {
+  title: "GraphQl updated",
+  body: `Updated graphql at ${new Date()} !!`
+})
+  .then(user => {
+    console.log(JSON.stringify(user, undefined, 2));
+  })
+  .catch(error => console.log(error.message));
 
 // createPostForUser("cjqt9u4zp001n0a31n96vixdz", {
 //   title: "GreatBooks to Read",
 //   body: "The war of art",
 //   published: true
-// }).then(user => {
-//   console.log(JSON.stringify(user, undefined, 2));
-// });
+// })
+//   .then(user => {
+//     console.log(JSON.stringify(user, undefined, 2));
+//   })
+//   .catch(error => {
+//     console.log(error.message);
+//   });
